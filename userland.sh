0da362ce18dd70c7d847542ccf6a368cfc13ed22
Leonidas-source <66934329+Leonidas-source@users.noreply.github.com>
2021-01-27 23:23:04 +0700

    Add files via upload

---
+++ userland.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+xbps-install nano
+nano /etc/hostname
+echo "set root password"
+passwd
+xbps-install grub-x86_64-efi
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id="Void"
+xbps-reconfigure -fa
