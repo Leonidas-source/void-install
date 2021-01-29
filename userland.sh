19f0718fb9d6bd29d7c214ff91681fe95616b997
Leonidas-source <66934329+Leonidas-source@users.noreply.github.com>
2021-01-29 16:39:39 +0700

    Update userland.sh

--- userland.sh
+++ userland.sh
@@ -2,6 +2,7 @@
 xbps-install -Syu
 xbps-install -y mc wget unzip nano
 nano /etc/hostname
+clear
 echo "set root password"
 passwd
 ls | grep -w "right" || (xbps-install grub-x86_64-efi && grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id="Void")
