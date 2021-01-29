ab674228c9990135b90b409a36de762f33d5ff15
Leonidas-source <66934329+Leonidas-source@users.noreply.github.com>
2021-01-29 07:28:20 +0700

    Delete userland.sh

--- userland.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-xbps-install -Syu 
-xbps-install -y mc wget unzip nano
-nano /etc/hostname
-echo "set root password"
-passwd
-xbps-install grub-x86_64-efi
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id="Void"
-xbps-reconfigure -fa
