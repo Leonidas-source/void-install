9ee1738e66e792eda89a621319d8a61b5e3c92a5
Leonidas-source <66934329+Leonidas-source@users.noreply.github.com>
2021-01-30 13:51:13 +0700

    Update userland.sh

--- userland.sh
+++ userland.sh
@@ -24,5 +24,5 @@ echo "install DE?
 2) no"
 read answr3
 [ "$answr3" == "1" ] && bash de.sh
-ls | grep -w "right" || (xbps-install grub-x86_64-efi && grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id="Void")
+ls | grep -w "right" || (xbps-install -y grub-x86_64-efi && grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id="Void")
 xbps-reconfigure -fa
